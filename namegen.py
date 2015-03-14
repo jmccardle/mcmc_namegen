ced245efd8f8ff2dc1a8df12bf4be74c5c5735b1
John McCardle <jmccardle@ffwf.net>
2015-03-14 19:30:27 -0400

    Fixed all the rookie bugs in the MarkovChain class

--- namegen.py
+++ namegen.py
@@ -8,12 +8,9 @@ class MarkovState:
         return str(self.transitions)
     def increment(self, ch):
         if ch in self.transitions:
-            #print "Self.transitions `{}` has `{}`; incrementing".format(self.transitions, ch)
             self.transitions[ch] += 1
         else:
-            #print "Self.transitions `{}` has NO `{}`; creating".format(self.transitions, ch)
             self.transitions[ch] = 1
-        #print('ch `{}` now has {} transitions'.format(ch, self.transitions[ch]))
     def transition(self):
         count = sum([self.transitions[key] for key in self.transitions])
         r = random.randint(0, count)
@@ -26,6 +23,7 @@ class MarkovState:
 
 class MarkovChain:
     def __init__(self, haltstate='\n', between=''):
+        self.haltstate = haltstate
         self.states = {haltstate: None} #Why no state? We need to get an error if we try to transition from the halt state.
         self.init = MarkovState()
         self.between = between
@@ -36,29 +34,34 @@ class MarkovChain:
         while state != self.haltstate:
             output.append(state)
             if maxlength > 0 and len(output) >= maxlength:
+                break
             nextstate = self.states[state].transition()
             state = nextstate
         return self.between.join(output)
 
-    def AddLink(linkstate, targetstate):
-        if not linkstate in self.states:
-            self.states[linkstate] = MarkovState()
+    def AddLink(self, linkstate, targetstate):
+        for s in (linkstate, targetstate):
+            if not s in self.states:
+                self.states[s] = MarkovState()
         if targetstate in self.states[linkstate].transitions:
             self.states[linkstate].transitions[targetstate] += 1
         else:
             self.states[linkstate].transitions[targetstate] = 1
 
-    def AddWord(word):  #Adds each letter pair in the word to the markov chain
+    def AddWord(self, word, terminate=False):  #Adds each letter pair in the word to the markov chain
+        if not word[0] in self.states:
+            self.states[word[0]] = MarkovState()
         self.init.increment(word[0])
-        for n in xrange(1, len(word)-1):
+        for n in xrange(0, len(word)-1):
             self.AddLink(word[n], word[n+1])
-        self.AddLink(word[-1], self.haltstate)
+        if terminate:
+            self.AddLink(word[-1], self.haltstate)
 
     def AddWords(sentence): #Adds each word pair in the sentence to the markov chain
         pass
         
-states = {'\n': None}
-init = MarkovState()
+#states = {'\n': None}
+#init = MarkovState()
 
 def RandomWalk():
     state = init.transition()
@@ -97,19 +100,17 @@ def addNextLetter(l, nextl):
         states[l].transitions[nextl] = 1
     #print("The transition `{}` -> `{}` is now at weight {}".format(l, nextl, states[l].transitions[nextl]))
 
-def readFile(fn):
-    with open(fn, "r") as f:
-        for line in f:
-            line = line.lower()
-            if line[0] in init.transitions:
-                init.transitions[line[0]] += 1
-            else:
-                init.transitions[line[0]] = 1
-            for n in xrange(1, len(line)-1):
-                addNextLetter(line[n], line[n+1])
 
 
 if __name__ == '__main__':
+    mc = MarkovChain()
+
+    def readFile(fn):
+        global mc
+        with open(fn, "r") as f:
+            for line in f:
+                mc.AddWord(line)
+
     if len(sys.argv) > 1:
         readFile(sys.argv[1])
     else:
@@ -117,17 +118,18 @@ if __name__ == '__main__':
         txt = '_'
         while txt != '\n':
             txt = raw_input() + '\n'
-            if txt[0] in init.transitions:
-                init.transitions[txt[0]] += 1
-            else:
-                init.transitions[txt[0]] = 1
-            for n in xrange(1, len(txt)-1):
-                addNextLetter(txt[n], txt[n+1])
+            mc.AddWord(txt)
+            #if txt[0] in init.transitions:
+            #    init.transitions[txt[0]] += 1
+            #else:
+            #    init.transitions[txt[0]] = 1
+            #for n in xrange(1, len(txt)-1):
+            #    addNextLetter(txt[n], txt[n+1])
 
 
-    print init
+    print mc.init
     print '\n'
-    print states
+    print mc.states
     for n in xrange(20):
-        print RandomWalk().strip('\n')
+        print mc.RandomWalk().strip('\n')
 
